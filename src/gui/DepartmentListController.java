400861bbe89c3eff4bf5b551c5e52e4ee0dcd095
Gabriel Dantas <gabrieldantaslp@gmail.com>
2022-04-16 12:19:22 -0300

    Department Service

--- src/gui/DepartmentListController.java
+++ src/gui/DepartmentListController.java
@@ -1,9 +1,12 @@
 package gui;
 
 import java.net.URL;
+import java.util.List;
 import java.util.ResourceBundle;
 
 import application.Main;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
@@ -12,8 +15,11 @@ import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.stage.Stage;
 import model.entities.Department;
+import model.services.DepartmentService;
 
 public class DepartmentListController implements Initializable{
+	// Criando uma dependencia
+	private DepartmentService service; // Será injetada sem implementação
 
 	@FXML
 	private TableView<Department> tableViewDepartment;
@@ -27,12 +33,16 @@ public class DepartmentListController implements Initializable{
 	@FXML
 	private Button btNew;
 	
+	private ObservableList<Department> obsList;
+	
 	@FXML
 	public void onBtNewAction() {
 		System.out.println("onBtNewAction");
 	}
-	
-	
+	// Metodo set para injetar a dependencia (Princípio Solid)
+	public void setDepartmentService(DepartmentService service) {
+		this.service = service;
+	}
 	
 	@Override
 	public void initialize(URL url, ResourceBundle rb) {
@@ -41,8 +51,6 @@ public class DepartmentListController implements Initializable{
 		
 	}
 
-
-
 	private void initializeNodes() {
 		// Inciando o comportamento das colunas
 		tableColumnId.setCellValueFactory(new PropertyValueFactory<>("id"));
@@ -50,6 +58,16 @@ public class DepartmentListController implements Initializable{
 		// Redimensionando o tamanho da tabela deparment
 		Stage stage = (Stage) Main.getMainScene().getWindow();
 		tableViewDepartment.prefHeightProperty().bind(stage.heightProperty());
+	}
+	// Metodo responsável por acessar o serviço, carregar os departamentos e jogar na ObservableList
+	public void updateTableView() {
+		// Garantindo que vamos injetar um service
+		if (service == null) {
+			throw new IllegalStateException("Service was null");
+		}
+		List<Department> list = service.findAll();
+		obsList = FXCollections.observableArrayList(list);
+		tableViewDepartment.setItems(obsList);
 	}
 
 }
